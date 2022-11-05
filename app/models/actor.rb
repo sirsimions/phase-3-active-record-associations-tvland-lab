c9c03b85ec2bbb822a896ec7d9f4c7484ebce78c
Simion <simion.okola@student.moringaschool.com>
2022-11-05 13:30:44 +0300

    Completed assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
-end
+    has_many :characters
+    has_many :shows, through: :characters
+    def full_name
+      "#{self.first_name} #{self.last_name}"
+    end
+    def list_roles
+      roles = self.characters.map do |char|
+        "#{char.name} - #{self.shows.find(char.show_id).name}"
+      end
+    end
+  end
